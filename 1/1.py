135219fb80229644ad47912e198183ef8da9374b
Skillhh <ortegon.giovanny@hotmail.com>
2019-05-07 01:29:42 -0500

    Agregando Inicio

--- 1/1.py
+++ 1/1.py
@@ -9,6 +9,12 @@ pygame.init()
 # Color de fondo
 gray = ( 126, 126, 126 )
 black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 200, 0)
+blue = (0, 0, 200)
+bright_red = (255, 0, 0)
+bright_green = (0, 255, 0)
+bright_blue = (0 , 0, 255)
 # Tamaño de ventana
 display_width=800
 display_height=600 
@@ -23,7 +29,91 @@ carimg=pygame.image.load('car1.jpg')
 backgroundpic=pygame.image.load("download12.jpg")
 yellow_strip=pygame.image.load("yellow-strip.jpg")
 strip = pygame.image.load("strip.jpg")
+intro_background = pygame.image.load("background.jpg")
+instruction_background = pygame.image.load("background2.jpg")
 car_width=56
+
+
+def intro_loop():
+	intro = True
+	while intro:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+				sys.exit()
+		gamedisplays.blit(intro_background, (0 , 0))
+		largetext = pygame.font.Font('freesansbold.ttf', 115)
+		TextSurf, TextRect = text_objects("CAR GAME", largetext)
+		TextRect.center = (400, 100)
+		gamedisplays.blit(TextSurf, TextRect)
+		button("START", 150, 520, 100, 50, green, bright_green, "play")
+		button("QUIT", 550, 520, 100, 50, red, bright_red, "quit")
+		button("INSTRUCTION", 300, 520, 200, 50, blue, bright_blue, "intro")
+		pygame.display.update()
+		clock.tick(50)
+
+
+def button(msg, x, y, w, h, ic, ac, action=None):
+	mouse = pygame.mouse.get_pos()
+	click = pygame.mouse.get_pressed()
+	if x + w > mouse[0] > x and y + h > mouse[1] > y:
+		pygame.draw.rect( gamedisplays, ac, (x, y, w, h))
+		if click[0] == 1 and action != None:
+			if action == "play":
+				game_loop()
+			elif action == "quit":
+				pygame.quit()
+				quit()
+				sys.exit()
+			elif action == "intro":
+				introduction()
+			elif action == "menu":
+				intro_loop()
+	else:
+		pygame.draw.rect(gamedisplays, ic, (x, y, w, h))
+	smalltext = pygame.font.Font("freesansbold.ttf", 20)
+	textsurf, textrect = text_objects(msg, smalltext)
+	textrect.center = (( x + ( w / 2)), ( y + ( h / 2)))
+	gamedisplays.blit(textsurf, textrect)
+
+
+def introduction():
+	introduction = True
+	while introduction:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+				sys.exit()
+		gamedisplays.blit(instruction_background, (0, 0))
+		largetext = pygame.font.Font('freesansbold.ttf', 80)
+		smalltext = pygame.font.Font('freesansbold.ttf', 20)
+		mediumtext = pygame.font.Font('freesansbold.ttf', 40)
+		textSurf, textRect = text_objects("This is an car game in which you need dodge the coming cars", smalltext)
+		textRect.center = ((350), (200))
+		TextSurf, TextRect = text_objects("INSTRUCTION", largetext)
+		TextRect.center = ((400), (100))
+		gamedisplays.blit(TextSurf, TextRect)
+		gamedisplays.blit(TextSurf, textRect)
+		stextSurf,stextRect = text_objects("ARROW LEFT : LEFT TURN", smalltext)
+		stextRect.center = ((150), (400))
+		hTextSurf,hTextRect = text_objects("ARROW RIGTH : RIGTH TURN", smalltext)
+		hTextRect.center = ((150), (450)) 
+		atextSurf,atextRect = text_objects("A : ACCELERATOR", smalltext)
+		atextRect.center = ((150), (500))
+		rtextSurf,rtextRect = text_objects("B : BRAKE", smalltext)
+		rtextRect.center = ((150), (550))
+		ptextSurf,ptextRect = text_objects("P : PAUSE", smalltext)
+		ptextRect.center = ((150), (350))
+		sTextSurf,sTextRect = text_objects("CONTROLS", mediumtext)
+		sTextRect.center = ((150), (300))
+		gamedisplays.blit(sTextSurf, sTextRect)
+		gamedisplays.blit(stextSurf, stextRect)
+		gamedisplays.blit(hTextSurf, hTextRect)
+		gamedisplays.blit(atextSurf, atextRect)
+		gamedisplays.blit(rtextSurf, rtextRect)
+		gamedisplays.blit(ptextSurf, ptextRect)
 
 
 def obstacle(obs_startx, obs_starty, obs):
@@ -43,6 +133,13 @@ def obstacle(obs_startx, obs_starty, obs):
 		obs_pic = pygame.image.load("car6.jpg")
 	gamedisplays.blit(obs_pic, (obs_startx, obs_starty))
 
+
+def score_system(passed, score):
+	font =  pygame.font.SysFont(None, 25)
+	text = font.render("  Passed " + str(passed), True, black)
+	score = font.render("  Score " + str(score), True, red)
+	gamedisplays.blit(text, (0, 50))
+	gamedisplays.blit(score, (0, 30))
 
 
 def text_objects(text, font):
@@ -100,6 +197,9 @@ def game_loop():
 	obs_starty = -750
 	obs_width = 56
 	obs_height = 125
+	passed = 0
+	level = 0
+	score = 0
 
 
 	bumped=False
@@ -114,6 +214,10 @@ def game_loop():
 				x_change = -5
 			if event.key == pygame.K_RIGHT:
 				x_change = 5
+			if event.key == pygame.K_a:
+				obstacle_speed += 2
+			if event.key == pygame.K_b:
+				obstacle_speed -=2
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
 				x_change = 0
@@ -126,6 +230,8 @@ def game_loop():
 		obstacle(obs_startx, obs_starty, obs)
 		obs_starty += obstacle_speed
 		car(x, y)
+		score_system(passed, score)
+
 		
 		if x > 690 - car_width or x < 110:
 			crash()
@@ -135,6 +241,17 @@ def game_loop():
 			obs_starty = 0 - obs_height
 			obs_startx = random.randrange( 170, (display_width - 170 ))
 			obs = random.randrange(0, 6)
+			passed = passed + 1
+			score = passed * 10
+			if int(passed) % 10 == 0:
+				level = level + 1
+				obstacle_speed + 2
+				largetext = pygame.font.Font("freesansbold.ttf", 80)
+				textsurf,textrect = text_objects("LEVEL "+str(level), largetext)
+				textrect.center=((display_width/2), (display_height/2))
+				gamedisplays.blit(textsurf, textrect)
+				pygame.display.update()
+				time.sleep(3)
 
 		if y < obs_starty + obs_height:
 			if x > obs_startx and x < obs_startx + obs_width or x + car_width > obs_startx and x + car_width < obs_startx + obs_width:
@@ -143,7 +260,7 @@ def game_loop():
 		pygame.display.update()
 		clock.tick(60)
 
-
+intro_loop()
 game_loop()
 pygame.quit()
 quit()
